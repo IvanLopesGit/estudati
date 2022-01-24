7663521383c65a8b36cb229280d38c154bba0960
Ivan <calintz1@hotmail.com>
2022-01-24 18:06:21 -0300

    inicio do studati + aula 1 python

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { Lesson1Component } from './components/courses/python/lesson1/lesson1.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProgramacaoComponent } from './components/programacao/programacao.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'programacao', component: ProgramacaoComponent},
+  {path: 'programacao/python/lesson1', component: Lesson1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProgramacaoComponent } from './components/programacao/programacao.component';
+import { Lesson1Component } from './components/courses/javascript/lesson1/lesson1.component';
+import { ComponentsComponent } from './components/components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    ProgramacaoComponent,
+    Lesson1Component,
+    ComponentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
